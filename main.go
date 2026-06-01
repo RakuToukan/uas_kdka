9299c827bc6237109b8bf8376125be2811d20443
r6rap <ahmadirafif7@gmail.com>
2026-06-01 22:35:10 +0700

    minkowski

--- main.go
+++ main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	"log"
-	"github.com/gin-gonic/gin"
-	"github.com/r6rap/uas_kdka/api/handler"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    "github.com/r6rap/uas_kdka/api/handler"
+    "time"
 )
 
 func main() {
-	r := gin.Default()
+    r := gin.Default()
 
-	r.Use(func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Content-Type")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	})
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"*"},
+        AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "ngrok-skip-browser-warning"},
+        ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+        MaxAge:           12 * time.Hour,
+    }))
 
-	r.GET("/categories", handler.GetCategories)
-	r.POST("/mosaic", handler.PostMosaic)
-	r.GET("/status/:id", handler.GetStatus)
+    r.GET("/categories", handler.GetCategories)
+    r.POST("/mosaic", handler.PostMosaic)
+    r.GET("/status/:id", handler.GetStatus)
 
-	log.Println("server running on :8080")
-	r.Run(":8080")
+    r.Run(":8080")
 }
